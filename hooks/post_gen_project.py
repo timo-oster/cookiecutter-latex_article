39559fcd57e16c1582808c59232aa469d8a53af3
Timo Oster <mail@timo-oster.net>
2018-07-12 15:39:40 +0200

    Fix hook commands

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,7 +5,7 @@ from subprocess import run, CalledProcessError
 
 try:
     run(["git", "init"], check=True)
-    run(["git" "config" "user.name" "{{cookiecutter.full_name}}"])
-    run(["git" "config" "user.email" "{{cookiecutter.email}}"])
+    run(["git", "config", "user.name", "{{cookiecutter.full_name}}"])
+    run(["git", "config", "user.email", "{{cookiecutter.email}}"])
 except CalledProcessError as e:
     sys.exit(1)
